f5f90522bc5f5b68c2980baa1a297e793165a97b
jlabung <labungjugal1@gmail.com>
2019-09-11 16:53:31 +0200

    init commit

---
+++ main.js
@@ -0,0 +1,23 @@
+const modal = document.querySelector(".modal_background");
+modal.addEventListener("click", () => {
+	modal.classList.add("hide");
+});
+
+
+const sheetID ="100qQoIcKg2L2TTRjjEwimTeGiT_wnrp66IskS4nIBOw";
+
+
+const link = `https://spreadsheets.google.com/feeds/list/${sheetID}/od6/public/values?alt=json`;
+
+fetch(link).then(res => res.json()).then(showVenue);
+
+function showVenue(data){
+	console.log(data);
+	const myArray = data.feed.entry;
+	myArray.forEach(showEvent);
+}
+
+function showEvent(venueData){
+	const template = document.querySelector("template").content;
+	const copy = template.cloneNode(true)
+}
